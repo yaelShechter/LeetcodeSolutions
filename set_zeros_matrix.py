1fcd6a14c4343bd96ec4ddfb0aa1dec6a70a5e04
Yael Shechter <42112839+yaelShechter@users.noreply.github.com>
2021-08-13 16:49:39 +0300

    Update set_zeros_matrix.py
    Could be refactored to look better in the future, but the code works.

--- set_zeros_matrix.py
+++ set_zeros_matrix.py
@@ -4,26 +4,26 @@ Given an m x n integer matrix matrix, if an element is 0, set its entire row and
 You must do it in place.
 """
 
-def setZeroes(matrix) -> None:
-    first_row = 0
+def set_zeros_matrix(matrix: List[List[int]]) -> None:
     first_col = 0
     m = len(matrix)
     n = len(matrix[0])
-    for row in range(m):
-        for col in range(n):
-            if matrix[row][col] == 0:
-                if row == 0:
-                    first_row = 1
-                if col == 0:
-                    first_col = 1
-                else:
-                    matrix[row][0] = 0
-                    matrix[0][col] = 0
-    for row in reversed(range(1, m)):
-        for col in reversed(range(1, n)):
-            if matrix[row][0] == 0 or matrix[0][col] == 0:
-                matrix[row][col] = 0
-    if first_row:
+
+    for i in range(m):
+        if matrix[i][0] == 0:
+            first_col = 1
+            
+        for j in range(1, n):
+            if matrix[i][j] == 0:
+                matrix[i][0] = 0
+                matrix[0][j] = 0
+
+    for i in reversed(range(1, m)):
+        for j in reversed(range(1, n)):
+            if matrix[i][0] == 0 or matrix[0][j] == 0:
+                matrix[i][j] = 0
+
+    if matrix[0][0] == 0:
         matrix[0] = [0] * n
     if first_col:
         for i in range(m):
